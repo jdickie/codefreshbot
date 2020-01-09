b4ba5297ffaa53ac2c30f9111abec1dac4ee6952
Grant Dickie <gdickie@npr.org>
2020-01-09 16:42:18 -0500

    feat(Adding in web and events api for slack - working on getting it stood up locally)

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const { createEventAdapter } = require('@slack/events-api');
 const { Codefresh, Config } = require('codefresh-sdk');
 const configjson = require('config');
 const express = require('express');
 
 const CODEFRESH_AUTH_TOKEN = configjson.get('codefresh.token');
+const SLACK_SIGNING_SECRET = configjson.get('slack.signing_secret');
 
+const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET);
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hi");
+slackEvents.on('url_verification', (event) => {
+    try {
+        console.log(event);
+        if (!event.challenge) {
+            console.log("No challenge present - exiting");
+            next();
+        }
+        res.set('Content-Type', 'text/plain').status(200).send(event.challenge);
+        next();
+    } catch {
+        console.log("error responding to challenge");
+        // don't continue
+        res.send(400);
+    }
+
 });
+
+app.post('/events', slackEvents.requestListener());
 
 async function getCodefreshSDK() {
     return new Codefresh(await Config.load({
@@ -19,14 +36,8 @@ async function getCodefreshSDK() {
     }));
 };
 
-const sdk = getCodefreshSDK();
-
-async function getHelmSetup() {
-    let { stdout, stderr } = await exec(`export HELM_REPO_ACCESS_TOKEN="${CODEFRESH_AUTH_TOKEN}" && echo $HELM_REPO_ACCESS_TOKEN && helm repo add mycfrepo cm://h.cfcr.io/kostis-codefresh/default && helm search -l codefresh/cmss | head -n10`);
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-}
-
+let sdk = null;
 app.listen(configjson.get('port'), () => {
-    getCodefreshSDK();
+    sdk = getCodefreshSDK();
+    console.log("Online");
 });
