c9d8723f79addf9ceee7481600a5db8bf8769e79
Grant Dickie <gdickie@npr.org>
2020-02-26 16:45:54 -0500

    feat: Working through separating out pieces

--- handler.js
+++ handler.js
@@ -1,6 +1,14 @@
 'use strict';
 const handler = require('serverless-express/handler');
 const app = require('./lib/app');
+const cf_modal = require('./lib/cf_modal');
+const config = require('config');
 
+const projects = config.get('codefresh.projects');
 
+const deploy = async (event, context) => {
+    const trigger_id = event.body.trigger_id;
+    cf_modal.start(trigger_id);
+};
+exports.deploy = deploy;
 exports.api = handler(app);
